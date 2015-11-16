ba94a0d8723bb1f8d3de2ed6df18344757daba0a
Siwakorn Srisakaokul <sping128@gmail.com>
2015-11-15 22:18:56 -0600

    Added more testcases

--- Integration-of-Multiple-String-Solvers/src/edu/illinois/cs/iomss/Main/MainWrapper.java
+++ Integration-of-Multiple-String-Solvers/src/edu/illinois/cs/iomss/Main/MainWrapper.java
@@ -1,5 +1,13 @@
 package edu.illinois.cs.iomss.Main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class MainWrapper {
 
 	public static void main(String[] args) {
@@ -7,6 +15,34 @@ public class MainWrapper {
        	System.out.println("java.library.path = " + System.getProperty("java.library.path"));
        	System.setProperty("java.library.path", System.getProperty("java.library.path") + "blahh");
        	System.out.println("java.library.path = " + System.getProperty("java.library.path"));
+       	
+       	if (args.length != 1) {
+       		System.out.println("Need only one argument as an input file");
+       	} else {
+       		try {
+				Scanner cin = new Scanner(new File(args[0]));
+				List<String> lines = new ArrayList<String>();
+				while (cin.hasNextLine()) {
+					lines.add(cin.nextLine());
+				}
+				
+				cin.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+       	}
 	}
+	
+	public static void outputHAMPI(List<String> lines) throws FileNotFoundException {
+		String filename = "hampi_input.hmp";
+		PrintWriter out = new PrintWriter(new File(filename));
+		for (String line : lines) {
+			out.println(line);
+		}
+		System.out.println("Done generating an input for HAMPI");
+	}
+	
+	
 
 }
