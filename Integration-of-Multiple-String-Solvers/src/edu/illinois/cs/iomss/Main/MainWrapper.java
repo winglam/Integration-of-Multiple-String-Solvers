dd5f6e3a7290d0c66b8b6199b2f47876806ae42e
Siwakorn Srisakaokul <sping128@gmail.com>
2015-11-29 00:19:13 -0600

    Fixed tests

--- Integration-of-Multiple-String-Solvers/src/edu/illinois/cs/iomss/Main/MainWrapper.java
+++ Integration-of-Multiple-String-Solvers/src/edu/illinois/cs/iomss/Main/MainWrapper.java
@@ -12,10 +12,6 @@ public class MainWrapper {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       	System.out.println("java.library.path = " + System.getProperty("java.library.path"));
-       	System.setProperty("java.library.path", System.getProperty("java.library.path") + "blahh");
-       	System.out.println("java.library.path = " + System.getProperty("java.library.path"));
-       	
        	if (args.length != 1) {
        		System.out.println("Need only one argument as an input file");
        	} else {
